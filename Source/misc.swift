5c99ec120c30edb055227a356b77933d2f318252
Damian Kołakowski <kolakowski.damian@gmail.com>
2016-10-20 21:38:55 +0200

    Added html DSL. Added util class for parsing url encoded data.

--- Source/misc.swift
+++ Source/misc.swift
@@ -9,7 +9,20 @@ import Foundation
 
 public extension UInt8 {
     
-    public static var lf: UInt8 = 10, cr: UInt8 = 13, space: UInt8 = 32, colon: UInt8 = 58
+    public static var
+        lf: UInt8 = 10,
+        cr: UInt8 = 13,
+        space: UInt8 = 32,
+        colon: UInt8 = 58,
+        ampersand: UInt8 = 38,
+        lessThan: UInt8 = 60,
+        greaterThan: UInt8 = 62,
+        slash: UInt8 = 47,
+        equal: UInt8 = 61,
+        doubleQuotes: UInt8 = 34,
+        openingParenthesis: UInt8 = 40,
+        closingParenthesis: UInt8 = 41,
+        comma: UInt8 = 44
 }
 
 public struct Process {
